b0e453c4ee35784ec0fbaacec90a9fb6ddc40a53
Anna Paula <annamarquess.silva@gmail.com>
2023-01-31 17:27:41 -0300

    Commit Project

---
+++ src/test/java/dio/myprojectwebapi/MyProjectWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.myprojectwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
